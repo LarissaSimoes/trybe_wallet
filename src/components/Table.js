4ea61acdf9d2f0a231d74906199a55b8b28b4de7
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-03-15 15:00:15 -0300

    Req 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,46 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <caption>Tabela</caption>
+          <tr>
+            <th>Descrição</th>
+          </tr>
+          <tr>
+            <th>Tag</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Método de pagamento</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Valor</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Moeda</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Câmbio utilizado</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Valor convertido</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Moeda de conversão</th>
+            {/* <td>teste</td> */}
+          </tr>
+          <tr>
+            <th>Editar/Excluir</th>
+            {/* <td>teste</td> */}
+          </tr>
+        </table>
+      </div>
     );
   }
 }
