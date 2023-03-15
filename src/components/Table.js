6e5c5d49f2f7dd7e3bed8bd073f310dff87e209b
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-03-15 17:36:50 -0300

    Req 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
-          <caption>Tabela</caption>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
+          <thead>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                <td>
+                  { Number(expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2) }
+                </td>
+                <td>
+                  { Number(
+                    expense.value * expense.exchangeRates[expense.currency].ask,
+                  ).toFixed(2) }
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+  // dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
