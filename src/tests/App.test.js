d84dd48167f761832d8f9d75d6eaad657c748679
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-03-16 10:28:15 -0300

    Req 5 alterado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-// import { legacy_createStore as createStore } from 'redux';
-// import { Provider } from 'react-redux';
-// import { Router } from 'react-router-dom';
-// import { createMemoryHistory } from 'history';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-// import rootReducer from '../redux/reducers';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
 describe('Testes Trybe Wallet', () => {
+  const emailInput = 'email-input';
+  const passwordInput = 'password-input';
+
   test('Testando campos de e-mail, senha, botão entrar e exibições na tela da carteira', () => {
     renderWithRouterAndRedux(<App />);
 
-    const emailInput = 'email-input';
     expect(screen.getByTestId(emailInput)).toBeInTheDocument();
-
-    const passwordInput = 'password-input';
     expect(screen.getByTestId(passwordInput)).toBeInTheDocument();
-
     expect(screen.getByRole('button', { name: /entrar/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /entrar/i })).toBeDisabled();
 
     userEvent.type(screen.getByTestId(emailInput), 'larissa@hotmail.com');
     userEvent.type(screen.getByTestId(passwordInput), '123456');
+    expect(screen.getByRole('button', { name: /entrar/i })).toBeEnabled();
     userEvent.click(screen.getByRole('button', { name: /entrar/i }));
 
     const emailField = 'email-field';
@@ -33,5 +29,24 @@ describe('Testes Trybe Wallet', () => {
 
     const currencyField = 'header-currency-field';
     expect(screen.getByTestId(currencyField)).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: /trybe wallet/i, level: 1 })).toBeInTheDocument();
+  });
+});
+
+describe('', () => {
+  const emailInput = 'email-input';
+  const passwordInput = 'password-input';
+  beforeEach(() => {
+    renderWithRouterAndRedux(<App />);
+  });
+
+  test('', async () => {
+    userEvent.type(screen.getByTestId(emailInput), 'larissa@hotmail.com');
+    userEvent.type(screen.getByTestId(passwordInput), '123456');
+    userEvent.click(screen.getByRole('button', { name: /entrar/i }));
+    userEvent.type(screen.getByTestId('value-input'), '10');
+    userEvent.click(screen.getByRole('button', { name: /adicionar despesa/i }));
+    await waitFor(() => expect(screen.getByTestId('delete-btn')).toBeInTheDocument());
   });
 });
